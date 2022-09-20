59be79cd41f7c9fe5367d0c1213bbd2dd707bb3a
selvagit <selvagcp2020@gmail.com>
2022-09-20 22:00:28 +0530

    Added test for service pipeline

--- test/pipeline.test.ts
+++ test/pipeline.test.ts
@@ -2,6 +2,8 @@ import * as Pipeline from "../lib/pipeline-stack";
 import { App } from "aws-cdk-lib";
 import { Match, Template,} from "aws-cdk-lib/assertions";
 import { CfnDisk } from "aws-cdk-lib/aws-lightsail";
+import { ServiceStack } from "../lib/service-stack";
+import { PipelineStack } from "../lib/pipeline-stack";
 
 
 test ('Pipeline Stack', ()=> {
@@ -10,4 +12,26 @@ test ('Pipeline Stack', ()=> {
     const stack = new Pipeline.PipelineStack(app, "myTestStack");
     // then
     expect(Template.fromStack(stack).toJSON()).toMatchSnapshot();
+});
+
+test("Adding service stage", () => {
+    // GIVEN
+    const app = new App();
+    const serviceStack = new ServiceStack(app, "ServiceStack");
+    const pipelineStack = new PipelineStack(app, "PipelineStack");
+  
+    // WHEN
+    pipelineStack.addServiceStage(serviceStack, "Test");
+  
+    // THEN
+
+    Template.fromStack(pipelineStack).hasResourceProperties("AWS::CodePipeline::Pipeline",
+        {
+            stages: Match.arrayWith([
+                Match.objectLike({
+                    Name: "Test",
+                })
+            ])
+        }
+    )
 });
