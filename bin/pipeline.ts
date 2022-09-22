e80c84c3bc92de4197f237f7f20abaada47b72e9
selvagit <selvagcp2020@gmail.com>
2022-09-22 08:12:04 +0530

    Removed integrtion test addition

--- bin/pipeline.ts
+++ bin/pipeline.ts
@@ -8,9 +8,9 @@ const app              = new App();
 const pipelineStack    = new PipelineStack(app, 'PipelineStack', {});
 const serviceStackTest = new ServiceStack (app, 'ServiceStackTest',{stageName: "Test",})
 const serviceStackProd = new ServiceStack (app, 'ServiceStackProd',{stageName: "Prod",});
-const testStage        = pipelineStack.addServiceStage(serviceStackTest, "Test");
+//const testStage        = pipelineStack.addServiceStage(serviceStackTest, "Test");
 
-//pipelineStack.addServiceStage(serviceStackTest, "Test");
+pipelineStack.addServiceStage(serviceStackTest, "Test");
 pipelineStack.addServiceStage(serviceStackProd, "Prod");
-pipelineStack.addServiceIntegrationTestToStage(
-    testStage, serviceStackTest.serviceEndpointOutput.importValue);
+//pipelineStack.addServiceIntegrationTestToStage(
+//    testStage, serviceStackTest.serviceEndpointOutput.importValue);
